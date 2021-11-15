3d41129a5ee4610cb4bfa1af233605ec6fa6a23b
jeuub <jeuub.bodur@gmail.com>
2021-11-16 01:30:24 +0700

    promocode error and messages

--- src/pages/AuthPage/components/SignUp/SignUp.tsx
+++ src/pages/AuthPage/components/SignUp/SignUp.tsx
@@ -105,6 +105,7 @@ export const SignUp = () => {
   }
 
   const handleSubmit = async () => {
+    
 
     dispatch(loaderVisibilityHandler(true))
 
--- src/pages/DepositPage/components/SelectDeposit/SelectDeposit.tsx
+++ src/pages/DepositPage/components/SelectDeposit/SelectDeposit.tsx
@@ -4,7 +4,8 @@ import {Input} from "../../../../components/Input/Input";
 import {Button} from "../../../../components/Button/Button";
 import {ReactComponent as BitcoinIcon} from "../../img/btc-ico-white.svg";
 import {useTranslation} from "react-i18next";
-
+import { axiosClient } from "../../../../utils/axiosClient";
+import {modalService} from "../../../../services/modal/modalService";
 interface SelectDepositProps {
   onChangePage: (value: string) => void;
 }
@@ -21,6 +22,17 @@ export const SelectDeposit = ({
     onChangePage('currency')
   }
 
+  const handlePromocodeSubmit = async () => {
+    try {
+      let answer = await axiosClient.post('/Profile/ActivatePromocode', { code: promocode })
+      console.log('answer ', answer);
+    } catch (e:any) {
+      console.log(e.data)
+    }
+    
+    
+  }
+
   return (
     <div className="deposit-container__content">
       <Card>
@@ -32,7 +44,7 @@ export const SelectDeposit = ({
           title={'Use promo code to refill your balance'}
         />
 
-        <Button primary>
+        <Button primary disabled={!Boolean(promocode)} onClick={handlePromocodeSubmit}>
           {t('Apply')}
         </Button>
       </Card>
